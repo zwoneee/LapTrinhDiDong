db26dd9e3bfd0d9c83f1d627dbd48ed4006df78f
nhu quynh <lydonganh44444@gmail.com>
2025-06-23 23:59:32 +0700

    Quynh - finish

---
+++ ECommerceSystem.GUI/wwwroot/js/admin-home.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    // Add todo
+    $('#addTodo').click(function () {
+        let todoText = $('#todoInput').val().trim();
+        if (todoText) {
+            let todoItem = `
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    ${todoText}
+                    <span class="text-danger delete-btn" style="cursor:pointer;">✖</span>
+                </li>`;
+            $('#todoList').append(todoItem);
+            $('#todoInput').val('');
+        }
+    });
+
+    // Remove todo
+    $(document).on('click', '.delete-btn', function () {
+        $(this).closest('li').remove();
+    });
+
+    // Add by pressing Enter
+    $('#todoInput').keypress(function (e) {
+        if (e.which === 13) {
+            $('#addTodo').click();
+        }
+    });
+});
