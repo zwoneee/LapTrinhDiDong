30385f9ab9b8b828f93e0b10716989ad0e684ef5
bangw31 <kocotenw@gmail.com>
2025-06-24 02:23:18 +0700

    giao dien user

--- ECommerceSystem.GUI/Controllers/HomeController.cs
+++ ECommerceSystem.GUI/Controllers/HomeController.cs
@@ -1,21 +1,50 @@
-﻿using ECommerceSystem.GUI.Models;
+﻿using ECommerceSystem.GUI.Apis;
+using ECommerceSystem.GUI.Models;
+using ECommerceSystem.Shared.DTOs.Product;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
 
 namespace ECommerceSystem.GUI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICategoryApi _categoryApi;
+        private readonly IProductApi _productApi;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICategoryApi categoryApi, IProductApi productApi)
         {
             _logger = logger;
+            _categoryApi = categoryApi;
+            _productApi = productApi;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
-            return View();
+            int pageSize = 9;
+
+            var categories = await _categoryApi.GetAllAsync();
+
+            var productResponse = await _productApi.GetProductsAsync(
+                search: null,
+                categoryId: null,
+                minPrice: null,
+                maxPrice: null,
+                sortBy: null,
+                promotion: null,
+                page: page,
+                pageSize: pageSize
+            );
+
+            ViewBag.Categories = categories;
+            ViewBag.CurrentPage = page;
+            ViewBag.TotalPages = (int)Math.Ceiling((double)productResponse.Total / pageSize);
+
+            return View(productResponse.Products);
         }
 
         public IActionResult Privacy()
